18560b734ec83c66faec6996cd0f538061008e6d
Andrei Kravchenko <epozine@inbox.ru>
2020-11-27 01:12:05 +0300

    Update build cache

--- scripts/build_rpr_cache.bat
+++ scripts/build_rpr_cache.bat
@@ -1,4 +1,6 @@
 set TOOL=%1
 if not defined TOOL set TOOL="C:\Program Files\Side Effects Software\Houdini 18.5.351\bin\husk.exe"
 
+mkdir ..\Work\Results\Houdini
+
 %TOOL% "C:\TestResources\HoudiniAssets\Basic\basic_1\basic_1.usda" -R RPR -V 9 -o "..\Work\Results\Houdini\cache_building.jpg" --res 720 480
