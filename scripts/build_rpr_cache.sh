71a1b2c1d763328dfda3161a16e5f01d2e772e59
Andrei Kravchenko <epozine@inbox.ru>
2020-12-04 18:10:16 +0300

    fix build cache

--- scripts/build_rpr_cache.sh
+++ scripts/build_rpr_cache.sh
@@ -4,4 +4,4 @@ TOOL=$1
 
 mkdir ../Work/Results/Houdini
 
-$TOOL "$CIS_TOOLS/../TestResources/HoudiniAssets/Basic/basic_1/basic_1.usda" -R RPR -V 9 -o "../Work/Results/Houdini/cache_building.jpg" --res 720 480
+$TOOL "$CIS_TOOLS/../TestResources/HoudiniAssets/build_cache/build_cache.usda" -R RPR -V 9 -o "../Work/Results/Houdini/cache_building.jpg" --res 720 480
