04db06b150d811c76d0ffa097260e016a7e110a5
Andrei Kravchenko <epozine@inbox.ru>
2021-02-18 20:07:52 +0300

    Change RES_PATH

--- scripts/build_rpr_cache.bat
+++ scripts/build_rpr_cache.bat
@@ -3,4 +3,4 @@ if not defined TOOL set TOOL="C:\Program Files\Side Effects Software\Houdini 18.
 
 mkdir ..\Work\Results\Houdini
 
-%TOOL% "C:\TestResources\rpr_usdplugin_autotests\build_cache\build_cache.usda" -R RPR -V 9 -o "..\Work\Results\Houdini\cache_building.jpg" --res 720 480
+%TOOL% "C:\TestResources\rpr_usdplugin_autotests_assets\build_cache\build_cache.usda" -R RPR -V 9 -o "..\Work\Results\Houdini\cache_building.jpg" --res 720 480
